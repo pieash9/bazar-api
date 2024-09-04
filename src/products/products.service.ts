2cbc59ce05466a71fd8741d161d003bc9df9bfa4
Md. Pieash Ahmed <pieash9@gmail.com>
2024-09-04 14:26:35 +0600

    added query filtering option for products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { CategoriesService } from 'src/categories/categories.service';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { OrderStatus } from 'src/orders/enums/order-status.enum';
+import dataSource from 'db/data-source';
 
 @Injectable()
 export class ProductsService {
@@ -30,8 +31,71 @@ export class ProductsService {
     return await this.productsRepository.save(product);
   }
 
-  async findAll(): Promise<ProductEntity[]> {
-    return await this.productsRepository.find();
+  async findAll(query: any): Promise<any> {
+    let filteredTotalProducts: number;
+    let limit: number;
+
+    if (!query.limit) {
+      limit = 4;
+    } else {
+      limit = query.limit;
+    }
+
+    const queryBuilder = dataSource
+      .getRepository(ProductEntity)
+      .createQueryBuilder('product')
+      .leftJoinAndSelect('product.category', 'category')
+      .leftJoin('product.reviews', 'review')
+      .addSelect([
+        'COUNT(review.id) AS reviewCount',
+        'AVG(review.rating)::numeric(10,2) AS avgRating',
+      ])
+      .groupBy('product.id, category.id');
+
+    const totalProducts = await queryBuilder.getCount();
+
+    if (query.search) {
+      const search = query.search;
+
+      queryBuilder.andWhere('product.title like :title', {
+        title: `%${search}%`,
+      });
+    }
+
+    if (query.category) {
+      queryBuilder.andWhere('category.id=:id', { id: query.category });
+    }
+
+    if (query.minPrice) {
+      queryBuilder.andWhere('product.price>=:minPrice', {
+        minPrice: query.minPrice,
+      });
+    }
+    if (query.maxPrice) {
+      queryBuilder.andWhere('product.price<=:maxPrice', {
+        maxPrice: query.maxPrice,
+      });
+    }
+
+    if (query.minRating) {
+      queryBuilder.andHaving('AVG(review.rating)>=:minRating', {
+        minRating: query.minRating,
+      });
+    }
+
+    if (query.maxRating) {
+      queryBuilder.andHaving('AVG(review.rating)<=:maxRating', {
+        maxRating: query.maxRating,
+      });
+    }
+
+    queryBuilder.limit(limit);
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset);
+    }
+    const product = await queryBuilder.getRawMany();
+    return product;
   }
 
   async findOne(id: number) {
---
+++ src/utility/interceptors/serialize.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+
+    const now = Date.now();
+    return next.handle().pipe(
+      map((data: any) => {
+        console.log('before response');
+        return data;
+      }),
+    );
+  }
+}
