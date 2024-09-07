8f97beadf4b76da084081d51f9c0194d374a6338
Md. Pieash Ahmed <pieash9@gmail.com>
2024-09-07 16:33:07 +0600

    changed db

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,11 +3,7 @@ import 'dotenv/config';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*{.ts,.js}'],
   logging: false,
